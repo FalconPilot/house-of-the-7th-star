d91746fd9139df9d6f7c4920be60f10c2d930588
FalconPilot <fpilot.github@gmail.com>
2019-12-21 16:41:23 +0100

    sync workdir

--- js/plugins/setAnimationFramerate.js
+++ js/plugins/setAnimationFramerate.js
@@ -1,8 +1,11 @@
 /*:
  * @plugindesc This plugin allows you to simply modify the in-game animations framerate.
  * @author FalconPilot
- * 
+ *
  * @param FPS
+ * @type number
+ * @max 60
+ * @min 10
  * @desc The amount of FPS (Frames Per Second) for in-game animations. Must be set between 10 and 60 (inclusive)
  * @default 15
 */
@@ -10,17 +13,14 @@
 (function() {
 
   // Return a number comprised between 'min' and 'max' params
-  function numberBoundaries (num, min, max) {
-    return num > max ? max : (num < min ? min : num);
-  };
 
   const pluginName = 'SetAnimationFramerate';
   const parameters = PluginManager.parameters(pluginName);
-  const _fps = Number(parameters.FPS || 15);
-  const fps = numberBoundaries(_fps, 10, 60);
+  const fps = Number(parameters.FPS || 15);
+  console.log(parameters);
+  console.log(fps);
 
   Sprite_Animation.prototype.setupRate = function () {
-    console.log(Math.ceil(60 / fps));
     this._rate = Math.ceil(60 / fps);
   };
 })();
